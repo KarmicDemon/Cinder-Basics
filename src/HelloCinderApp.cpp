81137f881fa244eaa2b2a1c00562ad781944cd6f
Chukwudi Derek Uche <uchederek@yahoo.com>
2017-05-06 12:01:59 -0400

    sigh

--- src/HelloCinderApp.cpp
+++ src/HelloCinderApp.cpp
@@ -9,26 +9,22 @@ using namespace std;
 class HelloCinderApp : public App {
   public:
 	void setup() override;
-	void mouseDown( MouseEvent event ) override;
+	void mouseDown(MouseEvent event) override;
 	void update() override;
 	void draw() override;
 };
 
-void HelloCinderApp::setup()
-{
+void HelloCinderApp::setup() {
 }
 
-void HelloCinderApp::mouseDown( MouseEvent event )
-{
+void HelloCinderApp::mouseDown(MouseEvent event) {
 }
 
-void HelloCinderApp::update()
-{
+void HelloCinderApp::update() {
 }
 
-void HelloCinderApp::draw()
-{
-	gl::clear( Color( 0, 0, 0 ) ); 
+void HelloCinderApp::draw() {
+	gl::clear(Color(0,0,0));
 }
 
-CINDER_APP( HelloCinderApp, RendererGl )
+CINDER_APP(HelloCinderApp, RendererGl)
